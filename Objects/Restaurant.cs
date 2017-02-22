a66561d46e9578a67d04f02e5906f23f05c23177
Keely Glenn and Nicole Sanders <keely+nicole@>
2017-02-22 09:21:57 -0800

    add all folders, Database.cs, Restaurant.cs, Cuisine.cs; add properties, constructors, and Save and GetAll methods to Restaurant.cs

---
+++ Objects/Restaurant.cs
@@ -0,0 +1,149 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace RestaurantApp
+{
+    public class Restaurant
+    {
+        private int _id;
+        private string _name;
+        private string _location;
+        private bool _delivery;
+        private int _cuisineId;
+
+        public Restaurant(string Name, string Location, bool Delivery, int CuisineId, int Id = 0)
+        {
+            _name = Name;
+            _location = Location;
+            _delivery = Delivery;
+            _cuisineId = CuisineId;
+            _id = Id;
+        }
+
+// ensures no doubles are created in table
+        public override bool Equals(System.Object otherResturant)
+        {
+            if (!(otherResturant is Restaurant))
+            {
+                return false;
+            }
+            else
+            {
+                Restaurant newRestaurant = (Restaurant) otherResturant;
+                bool idEquality = (this.GetId() == newRestaurant.GetId());
+                bool nameEquality = (this.GetName() == newRestaurant.GetName());
+                bool locationEquality = (this.GetLocation() == newRestaurant.GetLocation());
+                bool deliveryEquality = (this.GetDelivery() == newRestaurant.GetDelivery());
+                bool cuisineIdEquality = (this.GetCuisineId() == newRestaurant.GetCuisineId());
+
+                return (idEquality && nameEquality && locationEquality && deliveryEquality && cuisineIdEquality);
+            }
+        }
+// get id
+        public int GetId()
+        {
+            return _id;
+        }
+// get and set name
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string Name)
+        {
+            _name = Name;
+        }
+// get location
+        public string GetLocation()
+        {
+            return _location;
+        }
+// get delivery
+        public bool GetDelivery()
+        {
+            return _delivery;
+        }
+// get cuisine id
+        public int GetCuisineId()
+        {
+            return _cuisineId;
+        }
+
+// get all method for Restaurant list
+
+        public static List<Restaurant> GetAll()
+        {
+            List<Restaurant> AllRestaurants = new List<Restaurant>{};
+
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM restaurant;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                int restaurantId = rdr.GetInt32(0);
+                string restaurantName = rdr.GetString(1);
+                string restaurantLocation = rdr.GetString(2);
+                if (rdr.GetInt32(3) == 1)
+                {
+                    bool restaurantDelivery = true;
+                }
+                else
+                {
+                    bool restaurantDelivery = false;
+                }
+                int restaurantCusineId = rdr.GetInt32(4);
+                Restaurant newRestaurant = new Restaurant(restaurantId, restaurantName, restaurantLocation, restaurantDelivery, restaurantCusineId);
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+            return AllRestaurants;
+        }
+// save method for each restaurant
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO restaurant (name, location, delivery, cuisine_id) OUTPUT INSERTED.id VALUES (@RestaurantName, @RestaurantLocation, @restaurantDelivery, @RestaurantCuisineId);", conn);
+
+            SqlParameter nameParameter = new SqlParameter("@RestaurantName", this.GetName());
+
+            SqlParameter locationParameter = new SqlParameter("@RestaurantLocation", this.GetLocation());
+
+            SqlParameter deliveryParameter = new SqlParameter("@RestaurantDelivery", this.GetDelivery());
+
+            SqlParameter cuisineIdParameter = new SqlParameter("@RestaurantCuisineId", this.GetCuisineId());
+
+            cmd.Parameters.Add(nameParameter);
+            cmd.Parameters.Add(locationParameter);
+            cmd.Parameters.Add(deliveryParameter);
+            cmd.Parameters.Add(cuisineIdParameter);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+           {
+               this._id = rdr.GetInt32(0);
+           }
+           if (rdr != null)
+           {
+               rdr.Close();
+           }
+           if (conn != null)
+           {
+               conn.Close();
+           }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# Best Restaurants
+## by Keely Glenn and Nicole Sanders
+
+## Description
+
+This webpage will take user input about Restaurants and then sort that information according to different search requests
+
+## Setup and Installation
+
+* Clone repository
+* Open index.html in browser of your choice
+
+## Technologies Used
+
+* HTML
+* Bootstrap
+* CSS
+* C#
+* Nancy
+* SQL
+
+
+
+## Specs
+#### User can add Restaurants
+* Input: SUDOCIPE
+* Output: SUDOCIPE
+
+
+#### User can add Cuisine
+* Input: FUSION
+* Output:  FUSION
+
+
+#### User can sort by cuisine
+* Input: Search by cuisine
+* Output: list of all Restaurants in that cuisine
+
+#### User can delete individual Restaurant
+* Input: Delete Restaurant
+* Output: Restaurant is deleted
+
+#### User can delete cuisine with corresponding Restaurants
+* Input: Delete all mexican food
+* Output: Mexican Food category and Restaurant have been deleted
+
+## Legal
+MIT License
+
+Copyright (c) 2017 Keely Glenn and Nicole Sanders
